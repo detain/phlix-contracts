9065e8f89a032ced8610ccd0d4ea81e134a6d82b
Joe Huss <detain@interserver.net>
2026-07-09 06:05:44 -0400

    feat(P8-S2): add SyncPlay types for collaborative playback sessions (#16)

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -28,4 +28,5 @@ export * from './Recommendation';
 export * from './Collection';
 export * from './AccessSchedule';
 export * from './ProfileTag';
+export * from './SyncPlay';
 export * from './StreamSession';
